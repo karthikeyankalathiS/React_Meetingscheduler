2b66b40541a70576ee339f93106e88dd569d9ab3
karthikeyankalathiS <Karthikeyankalathis.s@vuedata.in>
2023-10-31 13:00:52 +0530

    second changes

--- src/App.js
+++ src/App.js
@@ -4,23 +4,14 @@ import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
  
 import Home from './components/Home';
 import Booking from './components/Booking';
-import Month from './components/Month';
-import Week from './components/week';
-import Today from './components/Today';
  
 function App() {
   return (
     <div>
       <BrowserRouter>
-      {/* <Routes>
-        <Route path='/booking' element={<Booking/>} />
-      </Routes> */}
       <Routes>
         <Route path='/' element={<Home/>} />
         <Route path='/Booking' element={<Booking/>} />
-        <Route path='/month' element={<Month/>} />
-        <Route path='/week' element={<Week/>} /> 
-        <Route path='/today' element={<Today/>}/>            
       </Routes>
       </BrowserRouter>         
     </div>
--- src/components/Home.js
+++ src/components/Home.js
@@ -3,88 +3,140 @@ import Navbar from "./Navbar";
 import axios from "axios";
 import "./style.css";
 
-function Home({ currentPath, openUpdateModal, deleteFunction }) {
+function Home({ currentPath, openUpdateModal }) {
+    const itemsPerPage = 10;
     const [data, setData] = useState([]);
-    const [loading, setLoading] = useState(true); 
+    const [loading, setLoading] = useState(true);
+    const [currentPage, setCurrentPage] = useState(1);
 
     useEffect(() => {
-        fetchAllData();
-    }, []);
+        fetchAllData(currentPath);
+    }, [currentPath]);
 
-    async function fetchAllData() {
+    
+
+    async function fetchAllData(path) {
         try {
-            const response = await axios.get('http://127.0.0.1:3112/home');
+            const response = await axios.get(`http://127.0.0.1:3112${path}`);
             const datas = response.data.data;
             console.log(datas);
             setData(datas);
-            setLoading(false); 
+            setLoading(false);
         } catch (error) {
             console.error('Error fetching data:', error);
-            setLoading(false); 
+            setLoading(false);
         }
     }
+
+    function deleteFunction(meetingId) {
+        if (window.confirm("Are you sure you want to delete this entry?")) {
+            fetch(`http://127.0.0.1:3112/delete/${meetingId}`, {
+                method: 'DELETE',
+            })
+                .then((response) => {
+                    if (response.ok) {
+                        alert("Booking deleted successfully");
+                        window.location.reload();
+                    } else {
+                        alert("Failed to delete booking");
+                    }
+                })
+                .catch((error) => {
+                    console.error("Error deleting booking:", error);
+                    alert("Internal Server Error");
+                });
+        }
+    }
+
+    const startIndex = (currentPage - 1) * itemsPerPage;
+    const endIndex = startIndex + itemsPerPage;
+    const itemsOnCurrentPage = data.slice(startIndex, endIndex);
 
     return (
         <div className="App">
             <Navbar />
             <div className="container mt-5">
                 <h5 className="heading text-center text-white">MEETING <span style={{ color: "#0DF1DB" }}> DASHBOARD</span></h5>
-                <div className="btn-group mb-3 ml">
-                    <a href="/today" className={`btn btn-secondary${currentPath === '/today' ? ' active' : ''}`}>Today</a>
-                    <a href="/week" className={`btn btn-secondary${currentPath === '/week' ? ' active' : ''}`}>Weekly</a>
-                    <a href="/month" className={`btn btn-secondary${currentPath === '/month' ? ' active' : ''}`}>Monthly</a>
+                <div className="btn-group mb-3">
+                    <button className={`btn btn-secondary${currentPath === '/today' ? ' active' : ''}`} onClick={() => fetchAllData('/today')}>
+                        Today
+                    </button>
+                    <button className={`btn btn-secondary${currentPath === '/week' ? ' active' : ''}`} onClick={() => fetchAllData('/week')}>
+                        Weekly
+                    </button>
+                    <button className={`btn btn-secondary${currentPath === '/month' ? ' active' : ''}`} onClick={() => fetchAllData('/month')}>
+                        Monthly
+                    </button>
                 </div>
-                {loading ? ( 
+
+                {loading ? (
                     <p>Loading...</p>
                 ) : (
-                    <table className="table table-dark">
-                        <thead>
-                            <tr>
-                                <th>S.No</th>
-                                <th>Room Name</th>
-                                <th>Organizer Name</th>
-                                <th>Start Time</th>
-                                <th>End Time</th>
-                                <th>Actions</th>
-                            </tr>
-                        </thead>
-                        
-                        <tbody>                            
-                            {data.length > 0 ? (
-                                data.map((item, index) => (                                    
-                                    <tr key={item.meetingId}>
-                                        <td>{index + 1}</td>
-                                        <td>{item.roomName}</td>
-                                        <td>{item.employeeName}</td>
-                                        <td>{new Date(item.startTime).toLocaleString()}</td>
-                                        <td>{new Date(item.endTime).toLocaleString()}</td>
-                                        <td>
-                                            {new Date(item.startTime) > new Date() && (
-                                                <button
-                                                    className="btn btn-primary"
-                                                    type="button"
-                                                    data-toggle="modal"
-                                                    onClick={() => openUpdateModal(item.meetingId)}
-                                                >
-                                                    Update
-                                                </button>
-                                            )}
-                                            <button
-                                                className="btn btn-danger"
-                                                onClick={() => deleteFunction(item.meetingId)}
-                                            >
-                                                Delete
-                                            </button>
-                                        </td>
-                                    </tr>
-                                ))
-                            ) : (
+                    <div>
+                        <table className="table table-dark">
+                            <thead>
                                 <tr>
-                                    <td colSpan="6">No records found</td>
+                                    <th>S.No</th>
+                                    <th>Room Name</th>
+                                    <th>Organizer Name</th>
+                                    <th>Start Time</th>
+                                    <th>End Time</th>
+                                    <th>Actions</th>
                                 </tr>
-                            )}
-                        </tbody>
-                    </table>
+                            </thead>
+                            <tbody>
+                                {itemsOnCurrentPage.length > 0 ? (
+                                    itemsOnCurrentPage.map((item, index) => (
+                                        <tr key={item.meetingId}>
+                                            <td>{index + 1}</td>
+                                            <td>{item.roomName}</td>
+                                            <td>{item.employeeName}</td>
+                                            <td>{new Date(item.startTime).toLocaleString()}</td>
+                                            <td>{new Date(item.endTime).toLocaleString()}</td>
+                                            <td>
+                                                {new Date(item.startTime) > new Date() && (
+                                                    <button
+                                                        className="btn btn-primary"
+                                                        type="button"
+                                                        data-toggle="modal"
+
+                                                        onClick={() => openUpdateModal(item.meetingId)}
+                                                    >
+                                                        Update
+                                                    </button>
+                                                )}
+                                                <button
+                                                    className="btn btn-danger"
+                                                    onClick={() => deleteFunction(item.meetingId)}
+                                                >
+                                                    Delete
+                                                </button>
+                                            </td>
+                                        </tr>
+                                    ))
+                                ) : (
+                                    <tr>
+                                        <td colSpan="6">No records found</td>
+                                    </tr>
+                                )}
+                            </tbody>
+                        </table>
+                        <div className="pagination-controls">
+                            <button className="btn btn-primary"
+                                onClick={() => setCurrentPage(currentPage - 1)}
+                                disabled={currentPage === 1}
+                            >
+                                Previous
+                            </button>
+                            <span style={{ color: "white" }}>Page {currentPage}</span>
+                            <button className="btn btn-primary"
+                                onClick={() => setCurrentPage(currentPage + 1)}
+                                disabled={itemsOnCurrentPage.length < itemsPerPage}
+                            >
+                                Next
+                            </button>
+                        </div>
+                    </div>
                 )}
             </div>
         </div>
